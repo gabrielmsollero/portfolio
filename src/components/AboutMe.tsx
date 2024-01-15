77ca88319ff2858894713133d9829c3b1b1c11ce
Gabriel <sollero.gms@gmail.com>
2024-01-15 10:22:11 -0300

    Formatting AboutMe section.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,24 +1,36 @@
+import classes from "./AboutMe.module.css";
+
+import profile from '../assets/profile.png'
+
 export default function AboutMe() {
   return (
     <section>
-      <h1>Welcome to my portfolio!</h1>
-      <p>Hi there! My name is Gabriel and I'm a fullstack developer.</p>
-      <p>
-        I am passionate about software engineering, innovation and technology in
-        general. I've been working with software development since 2021, and
-        went through frontend, backend and fullstack positions (I'll also do
-        some devops if you need too), and I'm pretty happy working with any of
-        these. In the end of the day, what I really like is to see softwares
-        growing up and working smoothly!
-      </p>
-      <p>
-        I like motorcycles, martial arts and beer as well. Feel free to invite
-        me to anything that involves at least one of these and I'll be there!
-      </p>
-      <p>
-        Hey, since you've read this far, scroll down and you'll get to know a
-        little more about my education and professional experiences!
-      </p>
+      <div className={classes.content}>
+        <div className={classes.textWrapper}>
+          <h1>Welcome to my portfolio!</h1>
+          <p>Hi there! My name is Gabriel and I'm a fullstack developer.</p>
+          <p>
+            I am passionate about software engineering, innovation and technology in
+            general. I've been working with software development since 2021, and
+            went through frontend, backend and fullstack positions (I'll also do
+            some devops if you need too), and I'm pretty happy working with any of
+            these. In the end of the day, what I really like is to see softwares
+            growing up and working smoothly!
+          </p>
+          <p>
+            I like motorcycles, martial arts and beer as well. Feel free to invite
+            me to anything that involves at least one of these and I'll be there!
+          </p>
+          <p>
+            Hey, since you've read this far, scroll down and you'll get to know a
+            little more about my education and professional experiences!
+          </p>
+        </div>
+        <div className={classes.avatarWrapper}>
+          <div className={classes.bgCircle} />
+          <img src={profile} alt="My profile picture" />
+        </div>
+      </div>
     </section>
   );
 }
