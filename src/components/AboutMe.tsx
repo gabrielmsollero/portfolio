fb4ce0a07ebcbff89b958c1b7a0bea96db2484e6
Gabriel <sollero.gms@gmail.com>
2024-01-15 13:01:17 -0300

    Adding framer motion animations to AboutMe section.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,35 +1,85 @@
+import { Variant, motion } from "framer-motion";
+
 import classes from "./AboutMe.module.css";
 
-import profile from '../assets/profile.png'
+import profile from "../assets/profile.png";
 
 export default function AboutMe() {
+  type AnimationSettings = {
+    variants: { hidden: Variant; visible: Variant };
+    transition: { duration: number };
+  };
+
+  const textSettings: AnimationSettings = {
+    variants: {
+      hidden: { opacity: 0, y: -30 },
+      visible: { opacity: 1, y: 0 },
+    },
+    transition: { duration: 0.5 },
+  };
+
+  const imageVariants = {
+    hidden: { opacity: 0, y: 200 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <section>
-      <div className={classes.content}>
-        <div className={classes.textWrapper}>
-          <h1>Hi there👋</h1>
-          <p>Welcome to my portfolio! My name is Gabriel and I'm a fullstack developer.</p>
-          <p>
-            I am passionate about software engineering, innovation and technology in
-            general. I've been working with software development since 2021, and
-            went through frontend, backend and fullstack positions (I'll also do
-            some devops if you need too), and I'm pretty happy working with any of
-            these. In the end of the day, what I really like is to see softwares
-            growing up and working smoothly!
-          </p>
-          <p>
-            I like motorcycles, martial arts and beer as well. Feel free to invite
-            me to anything that involves at least one of these and I'll be there!
-          </p>
-          <p>
-            Hey, since you've read this far, scroll down and you'll get to know a
-            little more about my education and professional experiences!
-          </p>
-        </div>
-        <div className={classes.avatarWrapper}>
-          <div className={classes.bgCircle} />
-          <img src={profile} alt="My profile picture" />
-        </div>
+      <div
+        className={classes.content}
+      >
+        <motion.div
+          className={classes.textWrapper}
+          transition={{ staggerChildren: 0.1 }}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.h1 {...textSettings}>
+            Hi there👋
+          </motion.h1>
+          <motion.p {...textSettings}>
+            Welcome to my portfolio! My name is Gabriel and I'm a fullstack
+            developer.
+          </motion.p>
+          <motion.p {...textSettings}>
+            I am passionate about software engineering, innovation and
+            technology in general. I've been working with software development
+            since 2021, and went through frontend, backend and fullstack
+            positions (I'll also do some devops if you need too), and I'm pretty
+            happy working with any of these. In the end of the day, what I
+            really like is to see softwares growing up and working smoothly!
+          </motion.p>
+          <motion.p {...textSettings}>
+            I like motorcycles, martial arts and beer as well. Feel free to
+            invite me to anything that involves at least one of these and I'll
+            be there!
+          </motion.p>
+          <motion.p {...textSettings}>
+            Hey, since you've read this far, scroll down and you'll get to know
+            a little more about my education and professional experiences!
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className={classes.avatarWrapper}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.div
+            className={classes.bgCircle}
+            variants={{
+              hidden: { scale: 0, opacity: 0 },
+              visible: { scale: 1, opacity: 1 },
+            }}
+            transition={{ duration: 0.8 }}
+
+          />
+          <motion.img
+            variants={imageVariants}
+            transition={{ duration: 0.8 }}
+            src={profile}
+            alt="My profile picture"
+          />
+        </motion.div>
       </div>
     </section>
   );
