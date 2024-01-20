012dc6439d53d31bb1143de1f8860899bebd27fd
Gabriel <sollero.gms@gmail.com>
2024-01-20 20:43:24 -0300

    Customizing Responsiveness section.

--- src/components/Responsiveness.tsx
+++ src/components/Responsiveness.tsx
@@ -1,10 +1,32 @@
+import WavesSpacer from "./Utils/Spacers/WavesSpacer";
+import QRCode from "react-qr-code";
+
+import classes from "./Responsiveness.module.css";
+
 export default function Responsiveness() {
   return (
-    <section>
-      <h2>Let's talk about responsiveness?</h2>
-      <p>
-        Did you enjoy the portfolio? Scan the QR code and check it out with your phone!
-      </p>
-    </section>
+    <>
+      <WavesSpacer colors={["#bf3564", "#ca4e77", "#d46589", "#de7b9c"]} />
+      <section className={classes.responsiveness}>
+        <div className={classes.content}>
+          <div className={classes.textWrapper}>
+            <h2>Let's talk about responsiveness?</h2>
+            <p>
+              Did you enjoy the portfolio? Scan the QR code and check it out with your
+              phone!
+            </p>
+          </div>
+          <div className={classes.qrCodeWrapper}>
+            <QRCode
+              value={window.location.href}
+              bgColor="#de7b9c"
+              fgColor="#f9f9f9"
+              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+            />
+          </div>
+        </div>
+      </section>
+      <WavesSpacer colors={["#bf3564", "#ca4e77", "#d46589", "#de7b9c"]} flipped />
+    </>
   );
 }
