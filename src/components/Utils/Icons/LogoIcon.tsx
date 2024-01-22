c5d343fb4aa35814cce8502b12f13ef4b913dada
Gabriel <sollero.gms@gmail.com>
2024-01-22 18:08:28 -0300

    Adding background to floating buttons.

--- src/components/Utils/Icons/LogoIcon.tsx
+++ src/components/Utils/Icons/LogoIcon.tsx
@@ -1,21 +1,31 @@
 export default function LogoIcon() {
   return (
-    <a href="#">
+    <a
+      href="#"
+      style={{
+        borderRadius: "50%",
+        width: "40px",
+        height: "40px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        stroke-width="2"
+        strokeWidth="2"
         stroke="currentColor"
         fill="none"
         strokeLinecap="round"
         strokeLinejoin="round"
         style={{
-          width: "auto",
-          height: "1.5rem",
-          padding: "1rem",
-          color: "var(--text-color)",
+          margin: "auto",
+          width: "60%",
+          height: "60%",
+
         }}
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
